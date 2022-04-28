852333ef13a2f339e853ef8b017f9735e83f5096
Xilotttr <maaglo322@gmai.com>
2022-04-28 16:14:28 +0400

    Homework

--- JS_HW_1/JS_HW_1.js
+++ JS_HW_1/JS_HW_1.js
@@ -71,5 +71,3 @@ function ages(){
     return 
 }     
 console.log(ages())
-console.log(ages())
-console.log(ages())
---
+++ JS_HW_2/JS_HW_2.js
@@ -0,0 +1,67 @@
+let input=document.querySelector('input');
+let button=document.querySelector("button");
+let result=document.querySelector('.result_field')
+
+
+button.addEventListener("click", ()=> verification(input.value))
+
+function verification(str){
+    result.innerHTML='';
+    try{
+        str_check(str);
+        Length(str);
+        letters_check(str);
+        dog_check(str);
+        num_check(str);
+        
+        throw 'Verification done'
+        
+    } catch(e){
+        result.append(e);
+    }
+}
+
+function Length(a){
+    if(a.length < 5){
+        console.log('Verification failed (Length of string less than 5 symbols)')
+        throw 'Verification failed (Legth of string less than 5 symbols)'
+    }
+    else if(a.length > 64){
+        console.log('Verification failed (Length of string greater than 64 symbols)')
+        throw 'Verification failed (Length of string greater than 64 symbols)'
+    }
+}
+
+function dog_check(a){
+    if(a.includes('@') != true){
+        console.log('Verification failed (There is no `@` in the string)')
+        throw 'Verification failed (There is no `@` in the string)'
+    }
+}
+function num_check(a){
+    if(!/\d/.test(a)){
+        console.log('Verification failed (There is no digits in the string)')
+        throw 'Verification failed (There is no digits in the string)'
+    }
+}
+
+function str_check(a){
+    if(a == ''){
+        console.log('Your string is empty')
+        throw 'Your string is empty'
+    }
+}
+
+function letters_check(a){
+    if(!/[a-zа-я]/.test(a)){
+        console.log('Verification failed (There is NO letters in the string)')
+        throw 'Verification failed (There is NO letters in the string)'
+    }
+
+    if(!/[A-FА-Я]/.test(a)){
+        console.log('Verification failed (There is NO uppercase letters in the string)')
+        throw 'Verification failed (There is NO uppercase letters in the string)'
+    }
+    
+}
+
