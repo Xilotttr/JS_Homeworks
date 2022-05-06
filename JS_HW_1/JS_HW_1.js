919a0f32a4bac551f9c2514fe55fae7880b55bc3
Xilotttr <maaglo322@gmai.com>
2022-05-06 13:57:27 +0400

    Homework

--- JS_HW_1/JS_HW_1.js
+++ JS_HW_1/JS_HW_1.js
@@ -175,41 +175,4 @@ ages_3(54);
 // 4***:
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 
-let age_4 = prompt('Enter your age');
 
-function ages_4(age){
-    let age_2 = 18, age_3 = 60
-    age = Number(age)
-    if(!isNaN(age) && age > 0)
-    {
-        if(age < age_2)
-        {
-            console.log (`You don't have access cause your age is ` + age + ` It's less then`)
-        }
-        else if(age >= age_2 && age < age_3)
-        {
-            console.log ('Welcome !')
-        }
-        else if(age > age_3)
-        {
-           console.log ('Keep calm and look Culture channel')
-        }
-        else
-        {
-            console.log ('Technical work')
-        }
-    }
-    else
-    {
-        if(isNaN(age))
-        {
-            console.log('Entered data is NaN')
-        }
-        else if(age < 0)
-        {
-            console.log('Entered data less than 0')
-        }
-    }  
-}     
-
-ages_4(age_4);
